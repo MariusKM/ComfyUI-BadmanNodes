443ad33a20935d4572db97409b2b67c3ed166669
MariusKM <m.morhard@hotmail.de>
2024-11-05 16:46:45 +0100

    updated init.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,14 @@ from .BadmanStrings import *
 from .BadmanImage import * 
 from .BadmanNumbers import * 
 from .BadmanConditioning import *
+from .BadmanColorTransfer import *
 
 
 NODE_CLASS_MAPPINGS = {
     "Badman_Blend": Blend,
+    "Badman_PalletteGenerator": RandomColorImageGrid,
     "Badman_HexGenerator": HexGenerator,
+    "Badman_ColorTransferLab": LabColorTransferNode,
     "Badman_String": BadmanString,
     "Badman_Concat_String": ConcatString,
     "Badman_Print": BadmanPrint,
@@ -14,16 +17,18 @@ NODE_CLASS_MAPPINGS = {
     "BadmanIntUtil" : BadmanIntUtil,
     "BadmanCLIPTextEncodeSDXLRegion" : BadmanCLIPTextEncodeSDXLRegion,
     "BadmanStringSelect" : SelectString,
+    
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Badman_Blend": "ImageBlend(Badman)",
+    "Badman_PalletteGenerator": "PalletteGenerator(Badman)",
     "Badman_HexGenerator": "HexGenerator(Badman)",
+    "Badman_ColorTransferLab" : "LABColorTransfer(Badman)",
     "Badman_String": "String (Badman)",
     "Badman_Concat_String": "Concat String (Badman)",
     "Badman_Print": "Print (Badman)",
     "Badman_IO": "IO Config (Badman)",
     "BadmanIntUtil": "Int Math (Badman)",
     "BadmanStringSelect": "Select String from List (Badman)",
-    "BadmanCLIPTextEncodeSDXLRegion" : "CLIPTextEncodeSDXLRegion (Badman)",
 }
